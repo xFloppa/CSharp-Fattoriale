eb570d6aec94508582c2c89fdbfcb350167d9cd6
xFloppa <floppa.strunz1234@gmail.com>
2024-01-09 15:56:54 +0100

    Tolti vari riferimenti

--- Fattoriale/Form1.Designer.cs
+++ Fattoriale/Form1.Designer.cs
@@ -50,7 +50,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
