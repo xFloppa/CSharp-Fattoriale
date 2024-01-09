818b9e9c0989f05f2680ee9e08ca47c41947fbe1
xFloppa <floppa.strunz1234@gmail.com>
2024-01-09 15:52:41 +0100

    Test

--- Fattoriale/Form1.Designer.cs
+++ Fattoriale/Form1.Designer.cs
@@ -67,7 +67,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
