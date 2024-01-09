2cd1adbc6882671b469ba51f3b30d5bdf8e44944
xFloppa <floppa.strunz1234@gmail.com>
2024-01-09 15:42:56 +0100

    First Commit

---
+++ Fattoriale/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fattoriale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num, i, fatt;
+            num = Convert.ToInt32(textBox1.Text);
+            fatt = 1;
+                for (i = 1; i <= num; i++){
+                fatt = fatt * i;
+                }
+            textBox2.Text = fatt.ToString();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
